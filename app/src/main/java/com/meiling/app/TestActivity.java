b9365fca1be3aff6b7c618267c488bd3dd626024
MeilingHong <marisareimu@126.com>
2022-01-24 10:50:33 +0800

    区分回调和调用参数根据是否设置进行设置参数；

--- app/src/main/java/com/meiling/app/TestActivity.java
+++ app/src/main/java/com/meiling/app/TestActivity.java
@@ -52,8 +52,42 @@ public class TestActivity extends AppCompatActivity {
         }
 
         @Override
-        public void noAudioRecordPermission() {
+        public void fileDirectoryEmptyOrNotExist() {
+            LogCatUtil.e("fileDirectoryEmptyOrNotExist");
+        }
+    };
+
+    private SystemCameraCall.CallbackVideo mVideoCallback = new SystemCameraCall.CallbackVideo() {
+        private File mTempFile;
+
+        @Override
+        public File getCurrentFile() {
+            return mTempFile;
+        }
+
+        @Override
+        public void setCurrentFile(File tempFile) {
+            mTempFile = tempFile;
+        }
+
+        @Override
+        public void noCameraHandler() {
+            LogCatUtil.e("noCameraHandler");
+        }
+
+        @Override
+        public void noCameraPermission() {
+            LogCatUtil.e("noCameraPermission");
+        }
+
+        @Override
+        public void noStoragePermission() {
             LogCatUtil.e("noStoragePermission");
+        }
+
+        @Override
+        public void noAudioRecordPermission() {
+            LogCatUtil.e("noAudioRecordPermission");
         }
 
         @Override
@@ -76,19 +110,35 @@ public class TestActivity extends AppCompatActivity {
                  *
                  * todo 更高版本系统有待验证
                  */
+                // todo 拍照权限检查
+//                if (result != null && result.getResultCode() == RESULT_OK) {
+//                    // 拍照执行成功
+//                    if (mCallback != null &&
+//                            // 检查文件存在与否
+//                            mCallback.getCurrentFile() != null &&
+//                            mCallback.getCurrentFile().exists() &&
+//                            mCallback.getCurrentFile().length() > 0) {
+//                        LogCatUtil.e("拍照执行成功：" + mCallback.getCurrentFile().getAbsolutePath());
+//                    } else {
+//                        LogCatUtil.e("拍照执行失败---文件不存在");
+//                    }
+//                } else {
+//                    LogCatUtil.e("拍照执行失败");
+//                }
+                // todo 摄像权限检查
                 if (result != null && result.getResultCode() == RESULT_OK) {
                     // 拍照执行成功
-                    if (mCallback != null &&
+                    if (mVideoCallback != null &&
                             // 检查文件存在与否
-                            mCallback.getCurrentFile() != null &&
-                            mCallback.getCurrentFile().exists() &&
-                            mCallback.getCurrentFile().length() > 0) {
-                        LogCatUtil.e("拍照执行成功：" + mCallback.getCurrentFile().getAbsolutePath());
+                            mVideoCallback.getCurrentFile() != null &&
+                            mVideoCallback.getCurrentFile().exists() &&
+                            mVideoCallback.getCurrentFile().length() > 0) {
+                        LogCatUtil.e("摄像执行成功：" + mVideoCallback.getCurrentFile().getAbsolutePath());
                     } else {
-                        LogCatUtil.e("拍照执行失败---文件不存在");
+                        LogCatUtil.e("摄像执行失败---文件不存在");
                     }
                 } else {
-                    LogCatUtil.e("拍照执行失败");
+                    LogCatUtil.e("摄像执行失败");
                 }
             }
         });
@@ -97,15 +147,27 @@ public class TestActivity extends AppCompatActivity {
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // todo 拍照权限检查
+//                if (!checkPermission() &&
+//                        Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                    requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+//                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//                            Manifest.permission.CAMERA}, 1000);
+//                    return;
+//                }
+//                SystemCameraCall.callSystemCamera_Picture(TestActivity.this, getDirectory(),
+//                        System.currentTimeMillis() + ".png", BaseConstant.authority, mCallback, activityResultLauncher);
+                // todo 摄像权限检查
                 if (!checkPermission() &&
                         Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
                             Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                            Manifest.permission.CAMERA}, 1000);
+                            Manifest.permission.CAMERA,
+                            Manifest.permission.RECORD_AUDIO}, 1000);
                     return;
                 }
-                SystemCameraCall.callSystemCamera_Picture(TestActivity.this, getDirectory(),
-                        System.currentTimeMillis() + ".png", BaseConstant.authority, mCallback, activityResultLauncher);
+                SystemCameraCall.callSystemCamera_Video(TestActivity.this, getDirectory(),
+                        System.currentTimeMillis() + ".png", BaseConstant.authority, 60, 1024 * 1024 * 10L, mVideoCallback, activityResultLauncher);
             }
         });
     }
--- base_app/src/main/java/com/meiling/utils/sys_camera/SystemCameraCall.java
+++ base_app/src/main/java/com/meiling/utils/sys_camera/SystemCameraCall.java
@@ -24,22 +24,38 @@ import androidx.core.content.FileProvider;
 public class SystemCameraCall {
 
     public interface Callback {
-        File getCurrentFile();// 用于返回
+        // 对于有连续调用需求的场景，建议对Callback实现类对象中声明一个File对象来获取
+        File getCurrentFile();// 用于返回照相的文件对象【主要用于在从】
 
-        void setCurrentFile(File tempFile);
+        void setCurrentFile(File tempFile);// 用了获取调用系统相机时，传入的文件对象
 
         void noCameraHandler();// 未找到可以处理动作的程序
 
-        void noCameraPermission();
+        void noCameraPermission();// 无相机权限
 
-        void noStoragePermission();
+        void noStoragePermission();// 无存储权限
 
-        void noAudioRecordPermission();
-
-        void fileDirectoryEmptyOrNotExist();
+        void fileDirectoryEmptyOrNotExist();// 目录对象为空或不存在【需要在调用前保证目录被创建出来】
     }
 
-    public static void callSystemCamera_Picture(@NonNull ComponentActivity mContext, File directory, String fileName, String authorityString, Callback callback, ActivityResultLauncher<Intent> activityResultLauncher) {
+    public interface CallbackVideo {
+        // 对于有连续调用需求的场景，建议对Callback实现类对象中声明一个File对象来获取
+        File getCurrentFile();// 用于返回照相的文件对象【主要用于在从】
+
+        void setCurrentFile(File tempFile);// 用了获取调用系统相机时，传入的文件对象
+
+        void noCameraHandler();// 未找到可以处理动作的程序
+
+        void noCameraPermission();// 无相机权限
+
+        void noStoragePermission();// 无存储权限
+
+        void noAudioRecordPermission();// 无录音权限
+
+        void fileDirectoryEmptyOrNotExist();// 目录对象为空或不存在【需要在调用前保证目录被创建出来】
+    }
+
+    public static void callSystemCamera_Picture(@NonNull ComponentActivity mContext, File directory, String fileName, String authorityString, Callback callback, @NonNull ActivityResultLauncher<Intent> activityResultLauncher) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {// 动态权限检查，保证拥有必要的权限
             if (mContext.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                 if (callback != null) callback.noCameraPermission();
@@ -73,13 +89,17 @@ public class SystemCameraCall {
                 String packageName = resolveInfo.activityInfo.packageName;
                 mContext.grantUriPermission(packageName, imageUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
             }
-            activityResultLauncher.launch(takePictureIntent);
+            activityResultLauncher.launch(takePictureIntent);// 使用启动对象启动跳转
         } else {
             if (callback != null) callback.noCameraHandler();// 未找到系统中可以处理该Intent的程序
         }
     }
 
-    public static void callSystemCamera_Video(@NonNull ComponentActivity mContext, File directory, String fileName, String authorityString, int videoTimeLimitSec, Callback callback, @NonNull ActivityResultLauncher<Intent> activityResultLauncher) {
+
+    public static void callSystemCamera_Video(@NonNull ComponentActivity mContext, File directory, String fileName, String authorityString,
+                                              Integer videoTimeLimitSec,
+                                              Long videoSizeBytes,
+                                              CallbackVideo callback, @NonNull ActivityResultLauncher<Intent> activityResultLauncher) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {// 动态权限检查，保证拥有必要的权限
             if (mContext.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                 if (callback != null) callback.noCameraPermission();
@@ -90,37 +110,38 @@ public class SystemCameraCall {
                 if (callback != null) callback.noStoragePermission();
                 return;
             }
+
             if (mContext.checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
                 if (callback != null) callback.noAudioRecordPermission();
                 return;
             }
         }
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-        if (takePictureIntent.resolveActivity(mContext.getPackageManager()) != null) {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(mContext.getPackageManager()) != null) {
             if (directory == null || !directory.exists() || !directory.isDirectory()) {// 保证目录存在，且不为空
                 if (callback != null) callback.fileDirectoryEmptyOrNotExist();
                 return;
             }
             File tempCameraFile = new File(directory, fileName);// 指定的存储文件
             if (callback != null) callback.setCurrentFile(tempCameraFile);
-
-            takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);// 授予访问权限
+            takeVideoIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);// 授予访问权限
             Uri imageUri = null;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 imageUri = FileProvider.getUriForFile(mContext, authorityString, tempCameraFile);// 在低版本系统上使用该方式，发现会存在文件无法访问的情况，导致使用系统相机拍摄后，无法进行预览【预览会抛出安全异常】
             } else {
                 imageUri = Uri.fromFile(tempCameraFile);
             }
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);// 设置输出文件
-            takePictureIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, videoTimeLimitSec);// 设置文件限制---时长：单位为秒
-            takePictureIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);// 设置文件限制---文件质量：此值在最低质量最小文件尺寸时是0，在最高质量最大文件尺寸时是１.
+            takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);// 设置输出文件
+            if (videoTimeLimitSec != null && videoTimeLimitSec > 0) takeVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, videoTimeLimitSec);// 设置文件限制---时长：单位为秒
+            takeVideoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);// 设置文件限制---文件质量：此值在最低质量最小文件尺寸时是0，在最高质量最大文件尺寸时是１.
+            if (videoSizeBytes != null && videoSizeBytes > 0) takeVideoIntent.putExtra(MediaStore.EXTRA_SIZE_LIMIT, videoSizeBytes);// 设置文件限制---文件大小：以字节为单位.
 
-            List<ResolveInfo> resInfoList = mContext.getPackageManager().queryIntentActivities(takePictureIntent, PackageManager.MATCH_DEFAULT_ONLY);
+            List<ResolveInfo> resInfoList = mContext.getPackageManager().queryIntentActivities(takeVideoIntent, PackageManager.MATCH_DEFAULT_ONLY);
             for (ResolveInfo resolveInfo : resInfoList) {
                 String packageName = resolveInfo.activityInfo.packageName;
                 mContext.grantUriPermission(packageName, imageUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
             }
-            activityResultLauncher.launch(takePictureIntent);// 使用启动对象启动跳转
+            activityResultLauncher.launch(takeVideoIntent);// 使用启动对象启动跳转
         } else {
             if (callback != null) callback.noCameraHandler();// 未找到系统中可以处理该Intent的程序
         }
